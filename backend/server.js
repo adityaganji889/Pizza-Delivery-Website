3252132dc588e5400c27b463b4a7754c0083aec2
Aditya Ganji <adityaganji889@gmail.com>
2023-07-09 00:47:41 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,32 @@
 const express = require("express");
 const app = express();
-require('dotenv').config()
-const dbConfig = require('./config/dbConfig')
 const userRoute = require("./routes/userRoutes")
 const pizzaRoute = require("./routes/pizzaRoutes")
 const orderRoute = require("./routes/orderRoutes")
+const path = require("path");
+require('dotenv').config()
+const dbConfig = require('./config/dbConfig')
+const port = process.env.PORT || 5000
 
-const PORT = process.env.PORT || 5000
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use("/api/users",userRoute)
 app.use("/api/pizzas",pizzaRoute)
 app.use("/api/orders",orderRoute)
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
+
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, "/frontend/build")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(_dirname, "/frontend/build/index.html"))
+});
+
+app.use((err, req, res, next)=>{
+    res.status(500).send({ message: err.message});
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`)
 })
